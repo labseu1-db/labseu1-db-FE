d657232546677e60406569ef910a9822618fac33
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-24 15:36:37 +0200

    Create db file

---
+++ src/Components/db.js
@@ -0,0 +1,14 @@
+const firebase = require('firebase/app');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyD59KY_n3nhHH1g1VhoBFs_xlYnnM0BgEU',
+  authDomain: 'labseu1-db.firebaseapp.com',
+  databaseURL: 'https://labseu1-db.firebaseio.com',
+  projectId: 'labseu1-db',
+  storageBucket: 'labseu1-db.appspot.com',
+  messagingSenderId: '952063624780'
+});
+const db = firebase.firestore();
+
+export default db;
