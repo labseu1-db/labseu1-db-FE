e415b7a6431f069713d52781bb0ee6053d7fb753
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-26 14:02:40 +0200

    Add firebase config for test database

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAnlNryT9tKH9im9-1nSobapAewvi3V3s8',
+  authDomain: 'labseu1-db-test.firebaseapp.com',
+  databaseURL: 'https://labseu1-db-test.firebaseio.com',
+  projectId: 'labseu1-db-test',
+  storageBucket: 'labseu1-db-test.appspot.com',
+  messagingSenderId: '280261803917'
+};
+
+export default firebaseConfig;
