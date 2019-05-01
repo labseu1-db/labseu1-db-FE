cbee1e827ff303d3233e631780a0b0ab075d8524
sean-attewell <sean.attewell@gmail.com>
2019-05-01 09:42:23 +0100

    folder structure and linter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 
-import AddCompany from './Components/AddCompany';
-import CompanyList from './Components/CompanyList';
-import AuthButton from './Components/AuthButton';
-
 class App extends Component {
   render() {
     return (
       <div>
-        <AuthButton />
-        <AddCompany />
-        <CompanyList />
+        text
       </div>
     );
   }
