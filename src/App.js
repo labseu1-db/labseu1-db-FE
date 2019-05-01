6b30c5fede56fa3b6561190721fef6119ab87161
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-01 20:42:02 +0200

    Add login animation

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import styled from 'styled-components';
 
 // import AddCompany from './Components/AddCompany';
 // import CompanyList from './Components/CompanyList';
-import SidebarEndpoint from './Endpoints/SidebarEndpoint';
-import HomeScreenEndpoint from './Endpoints/HomeScreenEndpoint';
-import UserProfileEndpoint from './Endpoints/UserProfileEndpoint';
-import OrganisationProfileEndpoint from './Endpoints/OrganisationProfileEndpoint';
-import SpaceEndpoint from './Endpoints/SpaceEndpoint';
-import ThreadEndpoint from './Endpoints/ThreadEndpoint';
-import CommentEndpoint from './Endpoints/CommentEndpoint';
+import LoginAnimation from './Components/animations/LoginAnimation';
 
 class App extends Component {
   render() {
     return (
       <SDApp>
-        <SidebarEndpoint />
-        <UserProfileEndpoint />
-        <OrganisationProfileEndpoint />
-        <SpaceEndpoint />
-        <ThreadEndpoint />
-        <CommentEndpoint />
-        <HomeScreenEndpoint />
+        <LoginAnimation />
       </SDApp>
     );
   }
