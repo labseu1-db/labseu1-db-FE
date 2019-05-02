0678c8764f93f8757c5b30e455f063fd57fa71b0
Thorben Bender <bender.thorben0@gmail.com>
2019-05-02 14:00:48 +0200

    created forgotPassword component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ForgotPassword from './Components/ForgotPassword'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ForgotPassword />
     </div>
   );
 }
