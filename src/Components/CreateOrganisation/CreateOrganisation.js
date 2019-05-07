4604c2c3a1a25d46cb13a6cd5725b19ffdfffa3c
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-07 16:50:00 +0200

    Create sending of data about organisation and spaces to firestore

--- src/Components/CreateOrganisation/CreateOrganisation.js
+++ src/Components/CreateOrganisation/CreateOrganisation.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose, bindActionCreators } from 'redux';
-import { firebaseConnect } from 'react-redux-firebase';
+import { firestoreConnect } from 'react-redux-firebase';
+import uuid from 'uuid';
 
 import { showModal } from '../../redux/actions/actionCreators';
 
@@ -28,9 +29,45 @@ class CreateOrganisation extends Component {
     this.setState({ createdSpaces: spaces });
   };
 
+  orgId = uuid();
+  addCompanyToDatabase() {
+    this.props.firestore.set(
+      { collection: 'companiesTEST', doc: this.orgId },
+      {
+        orgName: this.state.orgName,
+        // createdByUserId: _NEEDS_TO_BE_FILLED)WITH ID OF LOGGED IN USER,
+        isPremium: false,
+        arrayOfUsers: this.state.teamEmailAddress.filter(Boolean).map(e => {
+          return { userEmail: e };
+        })
+        //arrayOfAdmins: _NEEDS_TO_BE_FILLED)WITH ID OF LOGGED IN USER,
+      }
+    );
+  }
+
+  addSpacesToCompanies() {
+    this.state.createdSpaces.filter(Boolean).map(space => {
+      this.props.firestore.set(
+        { collection: 'companiesTEST', doc: uuid() },
+        {
+          orgId: this.orgId,
+          // spaceCreatedByUserId:  _NEEDS_TO_BE_FILLED)WITH ID OF LOGGED IN USER,
+          spaceName: space
+        }
+      );
+    });
+  }
+
   render() {
     return (
       <div>
+        <button
+          onClick={() => {
+            this.addCompanyToDatabase();
+            this.addSpacesToCompanies();
+          }}>
+          Submit
+        </button>
         {this.props.activeModal === 'CreateOrganisationModal' && (
           <CreateOrganisationModal shoudlBeOpen={true} showModal={this.props.showModal} activeModal={this.props.activeModal} addOrgName={this.addOrgName} />
         )}
@@ -44,7 +81,13 @@ class CreateOrganisation extends Component {
           />
         )}
         {this.props.activeModal === 'CreateSpacesModal' && (
-          <CreateSpacesModal shoudlBeOpen={true} showModal={this.props.showModal} activeModal={this.props.activeModal} addCreatedSpaces={this.addCreatedSpaces} />
+          <CreateSpacesModal
+            shoudlBeOpen={true}
+            showModal={this.props.showModal}
+            activeModal={this.props.activeModal}
+            addCreatedSpaces={this.addCreatedSpaces}
+            addCompanyToDatabase={this.addCompanyToDatabase}
+          />
         )}
         <button
           onClick={e => {
@@ -75,5 +118,5 @@ export default compose(
     mapStateToProps,
     mapDispatchToProps
   ),
-  firebaseConnect()
+  firestoreConnect()
 )(CreateOrganisation);
