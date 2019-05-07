55db47d5607f8f0447b06ab7e7fc7742dc1665e5
elisamartin <elisamv93@gmail.com>
2019-05-07 11:27:04 +0200

    Colors as variables in styled components

--- src/Components/styled-components/StyledButton.js
+++ src/Components/styled-components/StyledButton.js
@@ -4,33 +4,33 @@ export const StyledButton = styled.button`
 	font-size: 1.3rem;
 	width: 105px;
 	padding: 5px 15px;
-	background-color: #6c48f2;
+	background-color: var(--main-color);
 	color: white;
 	border-radius: 15px;
 	border: none;
 	cursor: pointer;
 	&:disabled {
-		background-color: lightgray;
+		background-color: var(--disabled-button);
 	}
 `;
 
 export const ForgotPasswordButton = styled.div`
 	font-family: 'Open Sans', sans-serif;
 	padding-left: 125px;
-	color: black;
+	color: var(--dgray-color);
 	font-size: 0.8rem;
 	font-weight: 300;
 	background: none;
 	border: none;
 	cursor: pointer;
 	&:hover {
-		color: #6c48f2;
+		color: var(--main-color);
 	}
 `;
 
 export const PasswordlessButton = styled.button`
 	font-family: 'Open Sans', sans-serif;
-	color: black;
+	color: var(--dgray-color);
 	font-size: 0.8rem;
 	font-weight: 300;
 	margin: 20px;
@@ -39,6 +39,6 @@ export const PasswordlessButton = styled.button`
 	padding: 0;
 	cursor: pointer;
 	&:hover {
-		color: #6c48f2;
+		color: var(--main-color);
 	}
 `;
--- src/Components/styled-components/StyledText.js
+++ src/Components/styled-components/StyledText.js
@@ -2,23 +2,25 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const StyledH1 = styled.h1`
-  font-family: 'Open Sans', sans-serif;
-  font-weight: 300;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 300;
+	color: var(--dgray-color);
 `;
 
 export const StyledLink = styled(Link)`
   font-family: 'Open Sans', sans-serif;
-  color: black;
+  color: var(--dgray-color);
   font-size: 0.9rem;
   font-weight: 300;
   &:hover {
-    color: #6c48f2;
+    color: var(--main-color);
   }
 `;
 
 export const StyledPLabel = styled.p`
-  font-family: 'Open Sans', sans-serif;
-  font-size: 0.9rem;
-  font-weight: 700;
-  padding-top: 5px;
+	font-family: 'Open Sans', sans-serif;
+	color: var(--dgray-color);
+	font-size: 0.9rem;
+	font-weight: 700;
+	padding-top: 5px;
 `;
