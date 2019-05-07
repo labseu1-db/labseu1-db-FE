989dcebcebcb141d7978aea10fb83bb1b7b0d07c
Samarvir Singh <samarvir1996@gmail.com>
2019-05-07 17:44:33 +0530

    Revert "Password eye icon change on sign up and login"

--- src/Components/styled-components/StyledLogin.js
+++ src/Components/styled-components/StyledLogin.js
@@ -1,62 +1,57 @@
 import styled from 'styled-components';
 
 export const StyledLogin = styled.div`
-	--main-color: #6c48f2;
-	--bg-color: #faf9f7;
-	--lgray-color: #bdc3c9;
-	--dgray-color: #3d4856;
-	--disabled-button: #cfd5f2;
-	margin: 0 auto;
-	width: 100%;
-	height: 100vh;
-	padding-top: 50px;
-	background-color: var(--bg-color);
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  padding-top: 50px;
+  background-color: #faf9f7;
 `;
 
 export const StyledLoginCon = styled.div`
-	height: 70%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const StyledForm = styled.form`
-	width: 450px;
-	padding: 30px 20px;
-	margin-bottom: 30px;
-	display: flex;
-	flex-direction: column;
-	background-color: white;
-	border-radius: 10px;
-	-webkit-box-shadow: 0px 15px 35px -34px rgba(92, 92, 91, 1);
-	-moz-box-shadow: 0px 15px 35px -34px rgba(92, 92, 91, 1);
-	box-shadow: 0px 15px 35px -34px rgba(92, 92, 91, 1);
+  width: 450px;
+  padding: 30px 20px;
+  margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 10px;
+  -webkit-box-shadow: 0px 15px 35px -34px rgba(92, 92, 91, 1);
+  -moz-box-shadow: 0px 15px 35px -34px rgba(92, 92, 91, 1);
+  box-shadow: 0px 15px 35px -34px rgba(92, 92, 91, 1);
 `;
 
 export const StyledInput = styled.input`
-	width: 70%;
-	border: none;
-	border-bottom: 2px solid var(--lgray-color);
-	padding: 5px 0;
-	margin-bottom: 10px;
-	&:focus {
-		border-bottom: 2px solid var(--main-color);
-		outline: none;
-	}
+  width: 70%;
+  border: none;
+  border-bottom: 2px solid lightgray;
+  padding: 5px 0;
+  margin-bottom: 10px;
+  &:focus {
+    border-bottom: 2px solid #6c48f2;
+    outline: none;
+  }
 `;
 
 export const StyledLabel = styled.div`
-	position: relative;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const StyledLowerSignIn = styled.div`
-	margin-top: 20px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const StyledLowerSignInPasswordless = styled.div`
@@ -67,11 +62,11 @@ export const StyledLowerSignInPasswordless = styled.div`
 `;
 
 export const StyledIcon = styled.img`
-	height: 16px;
-	width: 16px;
-	position: absolute;
-	top: 4px;
-	right: 0;
-	z-index: 2;
-	cursor: pointer;
+  height: 16px;
+  width: 16px;
+  position: absolute;
+  top: 4px;
+  right: 0;
+  z-index: 2;
+  cursor: pointer;
 `;
