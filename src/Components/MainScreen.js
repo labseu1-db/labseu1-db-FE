cf6b183b2111a5acde0f7b2038fc7c0f0848db2a
Thorben Bender <bender.thorben0@gmail.com>
2019-05-07 17:33:24 +0200

    added home, rightsidebar and mainscreen

---
+++ src/Components/MainScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function MainScreen() {
+    return (
+        <div>
+            <p>MainScreen</p>
+        </div>
+    )
+}
---
+++ src/Components/RightSidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function RightSidebar() {
+    return (
+        <div>
+            <p>RightSidebar</p>
+        </div>
+    )
+}
