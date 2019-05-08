d995c1ee4ef9118fa4dc1480fd4533cead6c9463
elisamartin <elisamv93@gmail.com>
2019-05-08 10:49:58 +0200

    Styling right side navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,49 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Icon }  from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 
 export default function NavBar() {
-    return (
-        <NavBarContainer>
-            <InnerContainer className="nav-bar-top">
-                <InnerContainer className="nav-bar-user-information">
-                    <img src="http://lorempixel.com/640/480" alt="user" />
-                    <InnerContainer>
-                      Full Name
-                    </InnerContainer>
-                </InnerContainer>
-                <InnerContainer className="notification-icons">
-                  <Icon name='cog'/> 
-
-                </InnerContainer>
-            </InnerContainer>
-            <InnerContainer className="nav-bar-main-section">
-                <InnerContainer className="home-screen-link">
-                    <Icon name='home'/> 
-                    <span>Home</span>
-                </InnerContainer>
-                <InnerContainer className="org-menu">
-                    <InnerContainer className="organisation-component">
-                      <InnerContainer className="organisation-name">Organisation name</InnerContainer>
-                      <InnerContainer className="space">space 1</InnerContainer>
-                      <InnerContainer className="space">space 2</InnerContainer>
-                      <InnerContainer className="space">space 3</InnerContainer>
-                      <InnerContainer className="space">space 4</InnerContainer>
-                    </InnerContainer>
-                </InnerContainer>
-            </InnerContainer>
-        </NavBarContainer>
-    )
+	return (
+		<NavBarContainer>
+			<InnerContainer className='nav-bar-top'>
+				<InnerContainer className='nav-bar-user-information'>
+					<img src='http://lorempixel.com/640/480' alt='user' />
+					<InnerContainer>Full Name</InnerContainer>
+				</InnerContainer>
+				<InnerContainer className='notification-icons'>
+					<Icon name='cog' />
+				</InnerContainer>
+			</InnerContainer>
+			<InnerContainer className='nav-bar-main-section'>
+				<InnerContainer className='home-screen-link'>
+					<Icon name='home' />
+					<span>Home</span>
+				</InnerContainer>
+				<InnerContainer className='org-menu'>
+					<InnerContainer className='organisation-component'>
+						<InnerContainer className='organisation-name'>Organisation name</InnerContainer>
+						<InnerContainer className='space'>space 1</InnerContainer>
+						<InnerContainer className='space'>space 2</InnerContainer>
+						<InnerContainer className='space'>space 3</InnerContainer>
+						<InnerContainer className='space'>space 4</InnerContainer>
+					</InnerContainer>
+				</InnerContainer>
+			</InnerContainer>
+		</NavBarContainer>
+	);
 }
 
 const InnerContainer = styled.div`
-  border: 1px solid pink;
-  padding: 5px;
-  `;
+	border: 1px solid pink;
+	padding: 5px;
+`;
 
 const NavBarContainer = styled.div`
-  border: 1px solid pink;
-  padding: 5px;
-  height: 100vh;
-  width: 15vw;
-  `;
+	border: 1px solid pink;
+	padding: 5px;
+	height: 100vh;
+	width: 15vw;
+`;
--- src/Components/RightSidebar.js
+++ src/Components/RightSidebar.js
@@ -1,10 +1,90 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import { Icon } from 'semantic-ui-react';
 
 export default function RightSidebar() {
-    return (
-        <div>
-            <p>RightSidebar</p>
-        </div>
-    )
+	return (
+		<StyledContainer>
+			<div>RightSidebar</div>
+			<GetStarted>
+				<GetStartedTop className='top'>
+					<p>Let's Get Started!</p>
+				</GetStartedTop>
+				<GetStartedCheckboxes>
+					<GetStartedCheck className='checkbox'>
+						<Icon name='check square outline' />
+						<p>Create an organization</p>
+					</GetStartedCheck>
+					<GetStartedCheck className='checkbox'>
+						<Icon name='check square outline' />
+						<p>Connect Slack</p>
+					</GetStartedCheck>
+					<GetStartedCheck className='checkbox'>
+						<Icon name='check square outline' />
+						<p>Invite Team Members</p>
+					</GetStartedCheck>
+				</GetStartedCheckboxes>
+			</GetStarted>
+
+			<LearnMore>
+				<p>Learn about Threads</p>
+				<LearnTopic>
+					<Icon name='check square outline' />
+					<p>What is a Thread?</p>
+				</LearnTopic>
+				<LearnTopic>
+					<Icon name='check square outline' />
+					<p>What are spaces and how do I use them?</p>
+				</LearnTopic>
+				<LearnTopic>
+					<Icon name='check square outline' />
+					<p>What threads should I write?</p>
+				</LearnTopic>
+			</LearnMore>
+		</StyledContainer>
+	);
 }
+
+const StyledContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	background-color: #faf9f7;
+	color: #3d4856;
+	font-weight: 700;
+	width: 100%;
+	height: 100%;
+	padding: 1%;
+`;
+const GetStarted = styled.div`
+	border: 1px solid lightgray;
+	border-radius: 15px;
+`;
+const GetStartedTop = styled.div`
+	background-color: #6c48f2;
+	border-radius: 15px 15px 0 0;
+	padding: 30px;
+	color: white;
+	text-align: end;
+	font-weight: 700;
+`;
+const GetStartedCheckboxes = styled.div`padding: 10px;`;
+const GetStartedCheck = styled.div`
+	background-color: white;
+	border-radius: 0 0 15px 15px;
+	display: flex;
+	padding: 5px;
+`;
+const LearnMore = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+const LearnTopic = styled.div`
+	border: 1px solid lightgray;
+	border-radius: 5px;
+	background-color: white;
+	display: flex;
+	padding: 15px;
+	margin-top: 5px;
+`;
