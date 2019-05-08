54722beaa4bac8fc3b80ea921d7820a096fe093a
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-08 10:14:50 +0200

    Create reusable ScreenHeading

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+//Import components
+import ScreenHeading from './reusable-components/ScreenHeading';
 
 export default function MainScreen() {
-    return (
-        <div>
-            <p>MainScreen</p>
-        </div>
-    )
+  return (
+    <div>
+      <ScreenHeading heading="Home" info="Catch up on the most recent threads." />
+    </div>
+  );
 }
---
+++ src/Components/reusable-components/ScreenHeading.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function ScreenHeading(props) {
+  const { heading, info } = props;
+  return (
+    <div>
+      <StyledHeadingContainer>
+        <div className="heading">{heading}</div>
+        <div className="info">{info}</div>
+      </StyledHeadingContainer>
+    </div>
+  );
+}
+
+//Stylin
+const StyledHeadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #374750;
+  font-family: Open Sans;
+  .heading {
+    font-weight: 300;
+    font-size: 3rem;
+    padding: 10px 0;
+    margin-bottom: 15px;
+  }
+  .info {
+    font-weight: 400;
+  }
+`;
+
+export default ScreenHeading;
