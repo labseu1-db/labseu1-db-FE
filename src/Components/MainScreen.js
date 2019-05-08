f0b8fb5f4c000fb2609d89dede562b4efcdbd0a5
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-08 10:39:39 +0200

    Create reusable screen button

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+//Import icons/images
+import penIconPurple from '../images/icon-pen-purple.svg';
+import penIconWhite from '../images/icon-pen-white.svg';
+
 //Import components
 import ScreenHeading from './reusable-components/ScreenHeading';
+import ScreenButton from './reusable-components/ScreenButton';
 
 export default function MainScreen() {
   return (
     <div>
       <ScreenHeading heading="Home" info="Catch up on the most recent threads." />
+      <ScreenButton content="Start a thread" icon={penIconWhite} backgroundColor="#5C4DF2" color="white" />
     </div>
   );
 }
