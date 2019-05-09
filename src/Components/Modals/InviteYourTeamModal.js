d81492471c4ff963381a5b8fb5f66158f73ada28
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-09 11:28:10 +0200

    Refactor invite team modal

--- src/Components/Modals/InviteYourTeamModal.js
+++ src/Components/Modals/InviteYourTeamModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Modal } from 'semantic-ui-react';
-import * as style from '../styled-components/StyledModal';
+import styled from 'styled-components';
 
 //Import components
 import ProgressBar from '../reusable-components/ProgressBar';
@@ -31,16 +31,16 @@ export default class InviteYourTeamModal extends Component {
     return (
       <Modal open={this.props.shoudlBeOpen} basic size="tiny">
         <ProgressBar activeDots={2} bulletpoints={3} />
-        <style.StyledModalH1>
+        <StyledModalH1>
           <Modal.Header content="Invite your team" />
-        </style.StyledModalH1>
-        <style.StyledModalCard>
+        </StyledModalH1>
+        <StyledModalCard>
           <Modal.Content>
-            <style.StyledModalForm>
-              <style.StyledModalLabel className="heading">Email addresses</style.StyledModalLabel>
+            <StyledModalForm>
+              <StyledModalLabel className="heading">Email addresses</StyledModalLabel>
               <div id="dynamicInput">
                 {this.state.inputs.map((input, i) => (
-                  <style.StyledModalInput
+                  <StyledModalInput
                     placeholder="Email address"
                     value={this.state.inputs[i]}
                     onChange={e => {
@@ -50,33 +50,120 @@ export default class InviteYourTeamModal extends Component {
                   />
                 ))}
               </div>
-            </style.StyledModalForm>
-            <style.StyledModalAdder onClick={() => this.appendInput()}>Add more emails</style.StyledModalAdder>
+            </StyledModalForm>
+            <StyledModalAdder onClick={() => this.appendInput()}>Add more emails</StyledModalAdder>
           </Modal.Content>
           <Modal.Actions>
-            <style.StyledActionButtonsContainer>
-              <style.StyledModalButton
+            <StyledActionButtonsContainer>
+              <StyledModalButton
                 onClick={e => {
                   e.preventDefault();
                   this.props.addTeamEmailAddress(this.state.inputs);
                   this.props.showModal('CreateSpacesModal');
                 }}>
                 Next
-              </style.StyledModalButton>
-              <style.StyledModalMainButtonContainer>
-                <style.StyledModalButton
+              </StyledModalButton>
+              <StyledModalMainButtonContainer>
+                <StyledModalButton
                   className="cancel-button"
                   onClick={e => {
                     e.preventDefault();
                     this.props.showModal('CreateSpacesModal');
                   }}>
                   Skip
-                </style.StyledModalButton>
-              </style.StyledModalMainButtonContainer>
-            </style.StyledActionButtonsContainer>
+                </StyledModalButton>
+              </StyledModalMainButtonContainer>
+            </StyledActionButtonsContainer>
           </Modal.Actions>
-        </style.StyledModalCard>
+        </StyledModalCard>
       </Modal>
     );
   }
 }
+
+const StyledModalH1 = styled.h1`
+  font-family: 'Open Sans', sans-serif;
+  text-align: center;
+  font-weight: 300;
+`;
+
+const StyledModalCard = styled.div`
+  background-color: white;
+  margin-top: 50px;
+  border-radius: 5px;
+  input:focus,
+  button:focus,
+  textarea:focus {
+    outline: none;
+  }
+`;
+
+const StyledModalForm = styled.form`
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 20px;
+  .heading {
+    padding-bottom: 40px;
+  }
+`;
+
+const StyledModalLabel = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1rem;
+  font-weight: 700;
+  padding-bottom: 10px;
+  color: black;
+  .ligther-font {
+    font-size: 0.8rem;
+    color: #bdc3c9;
+  }
+`;
+
+const StyledModalInput = styled.input`
+  width: 98%;
+  border: none;
+  border-bottom: 2px solid #bdc3c9;
+  padding: 10px 0 5px 0;
+  margin-bottom: 10px;
+  &::placeholder {
+    font-size: 1.2rem;
+  }
+`;
+
+const StyledModalButton = styled.button`
+  width: 100px;
+  padding: 5px 15px;
+  margin: 0 25px 25px 0;
+  color: white;
+  border-radius: 15px;
+  border: none;
+  cursor: pointer;
+  background-color: #5c4df2;
+`;
+
+const StyledActionButtonsContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: row-reverse;
+`;
+
+const StyledModalMainButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  .cancel-button {
+    color: #5c4df2;
+    background-color: white;
+    border: 2px solid #5c4df2;
+  }
+`;
+
+const StyledModalAdder = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1rem;
+  font-weight: 700;
+  color: #5c4df2;
+  padding: 0 25px;
+  cursor: pointer;
+`;
