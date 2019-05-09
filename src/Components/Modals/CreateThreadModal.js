0dcf61839aefee251719fb65861ee6a6a01fb027
elisamartin <elisamv93@gmail.com>
2019-05-10 01:06:34 +0200

    Added Editor, basic text styling working

--- src/Components/Modals/CreateThreadModal.js
+++ src/Components/Modals/CreateThreadModal.js
@@ -1,29 +1,73 @@
 import React, { Component } from 'react';
 import { Modal } from 'semantic-ui-react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 import styled from 'styled-components';
 
 import textCursor from '../../images/icon-cursor-purple.svg';
 
 export default class CreateThreadModal extends Component {
-  state = {
-    threadTitle: ''
-  };
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty(), threadTitle: '' };
+    this.onChange = (editorState) => this.setState({ editorState });
+    this.focus = () => this.refs.editor.focus();
+  }
+
   handleInputChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  };
+
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  };
+
+  onBoldClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  };
+
+  onItalicClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
   };
 
   render() {
     return (
       <Modal open={this.props.shoudlBeOpen} size='small'>
+        <div className='editor'>
+          <button onClick={this.onUnderlineClick}>U</button>
+          <button onClick={this.onBoldClick}>
+            <b>B</b>
+          </button>
+          <button onClick={this.onItalicClick}>
+            <em>I</em>
+          </button>
+
+          {/* <Editor editorState={this.state.editorState} onChange={this.onChange} handleKeyCommand={this.handleKeyCommand} /> */}
+        </div>
         <Modal.Content>
           <StyledInputsContainer>
-            <StyledTitleInput
-              name='threadTitle'
-              type='text'
-              placeholder='Create a title'
-              required
-              onChange={this.handleInputChange}
-            />
+            <StyledTitleInput onClick={this.focus}>
+              <Editor
+                name='threadTitle'
+                type='text'
+                placeholder='Create a title'
+                required
+                onChange={this.onChange}
+                editorState={this.state.editorState}
+                handleKeyCommand={this.handleKeyCommand}
+                ref='editor'
+              />
+            </StyledTitleInput>
+
             <StyledThreadInput
               name='threadTitle'
               type='text'
@@ -61,7 +105,7 @@ const StyledInputsContainer = styled.div`
   padding: 10px 30px;
   height: 500px;
 `;
-const StyledTitleInput = styled.input`
+const StyledTitleInput = styled.div`
   border: none;
   outline: none;
   width: 100%;
