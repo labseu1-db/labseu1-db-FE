369f17128b73616546bc1c15d07830a945e2086f
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-09 09:37:55 +0200

    Add placeholder and style it

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -6,6 +6,7 @@ import { firestoreConnect } from 'react-redux-firebase';
 
 //Import icons/images
 import penIconWhite from '../images/icon-pen-white.svg';
+import placeholder from '../images/placeholder-homescreen.svg';
 
 //Import components
 import ScreenHeading from './reusable-components/ScreenHeading';
@@ -15,7 +16,6 @@ import ThreadCard from './reusable-components/ThreadCard';
 
 //Main component
 function MainScreen(props) {
-  console.log(props.threads);
   return (
     <StyledMainScreen>
       <StyledFirstRow>
@@ -23,7 +23,19 @@ function MainScreen(props) {
         <ScreenButton content="Start a thread" icon={penIconWhite} backgroundColor="#5C4DF2" color="white" border="none" />
       </StyledFirstRow>
       <ScreenSectionHeading heading="Recent" />
-      {props.threads.length &&
+      {/*If not threads, show placeholder */}
+      {props.threads.length === 0 && (
+        <StyledPlaceholderContainer>
+          <StyledPlaceholderHeading>Learn about Home</StyledPlaceholderHeading>
+          <StyledPlaceholderInfo>Home is a great place where you fins all information about threads.</StyledPlaceholderInfo>
+          <StyledPlaceholderImage>
+            <img src={placeholder} alt="placeholder" />
+          </StyledPlaceholderImage>
+        </StyledPlaceholderContainer>
+      )}
+      {/*Loop trough all the threads that are associated with the orgId*/}
+      {/*OrgId is hardcoded -> we will need to fix this when we get id from logged in user*/}
+      {props.threads.length > 0 &&
         props.threads.map(t => {
           let dateInfo = new Date(t.threadCreatedAt.seconds * 1000);
           let date = `${dateInfo.getMonth()}/${dateInfo.getDate()} ${dateInfo.getHours()}:${dateInfo.getMinutes()}`;
@@ -45,6 +57,43 @@ const StyledFirstRow = styled.div`
   justify-content: space-between;
   align-items: flex-start;
   margin-bottom: 5vh;
+`;
+
+const StyledPlaceholderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledPlaceholderImage = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  margin-top: 50px;
+  img {
+    width: 40%;
+  }
+`;
+const StyledPlaceholderHeading = styled.div`
+  width: 100%;
+  color: white;
+  line-height: 2.5;
+  padding: 0 20px;
+  background-color: #5c4df2;
+  font-size: 1.1rem;
+  font-weight: 600;
+  border-radius: 10px 10px 0 0;
+`;
+
+const StyledPlaceholderInfo = styled.div`
+  width: 100%;
+  color: black;
+  background-color: #e6e5fe;
+  height: 100px;
+  padding: 20px;
+  border-radius: 0 0 10px 10px;
 `;
 
 //Export component wrapped in store + firestore
