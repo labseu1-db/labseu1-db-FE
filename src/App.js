bdc9ba47197aba2d63c41378f4df6bb86e1f5419
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-10 17:19:16 +0200

    Remoce createneworganisation route

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ class App extends Component {
         <Route exact path="/passwordlesssubmit" render={props => <PasswordlessSubmit {...props} />} />
         <Route exact path="/passwordlesscheck" render={props => <PasswordlessCheck {...props} />} />
         <Route exact path="/forgotPassword" render={props => <ForgotPassword {...props} />} />
-        <Route exact path="/createneworganisation" render={props => <CreateNewOrganisation {...props} />} />
       </div>
     );
   }
