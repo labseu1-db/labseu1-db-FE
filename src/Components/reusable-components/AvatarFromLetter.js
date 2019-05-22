34f2fd829baac1415eb48a967770463d57af58c1
Ivana Huckova <ivana.huckova@gmail.com>
2019-05-22 17:02:18 +0200

    WIP

--- src/Components/reusable-components/AvatarFromLetter.js
+++ src/Components/reusable-components/AvatarFromLetter.js
@@ -7,6 +7,7 @@ export default function AvatarFromLetter(props) {
     fontSize: props.fontSize,
     width: props.width,
     height: props.height,
+    // color: '#374650'
     color: 'white'
   };
   return <StyledImageContainer style={style}>{props.username[0].toUpperCase()}</StyledImageContainer>;
@@ -20,13 +21,14 @@ const StyledImageContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 1.3rem;
+  font-size: 1rem;
+  font-weight: 600;
   background-color: #ffc206;
   background-color: #dbc3b6;
   background-color: #fff7f3;
-  background-color: #e4d0c6;
-  background-color: #ffc9ad;
-  background-color: #ff8434;
-  background-color: #ff706c;
+  /* background-color: #e4d0c6; */
+  /* background-color: #ffc9ad; */
+  /* background-color: #ff8434;
+  background-color: #ff706c; */
   background-color: #ffb48f;
 `;
