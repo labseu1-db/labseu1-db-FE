2beeb72df10074cc1ec59f31994fa5feffe53e53
Thorben <bender.thorben@gmail.com>
2020-02-10 16:34:53 +0100

    added enviroment variables, changed travis.yml

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,16 +1,24 @@
 //Firebase configuration - test database
 const firebaseConfig = {
-  apiKey: 'AIzaSyAnlNryT9tKH9im9-1nSobapAewvi3V3s8',
-  authDomain: 'labseu1-db-test.firebaseapp.com',
-  databaseURL: 'https://labseu1-db-test.firebaseio.com',
-  projectId: 'labseu1-db-test',
-  storageBucket: 'labseu1-db-test.appspot.com',
-  messagingSenderId: '280261803917',
-  appId: '1:280261803917:web:3c02fd1e7ac4ae6a'
+  apiKey: "AIzaSyAnlNryT9tKH9im9-1nSobapAewvi3V3s8",
+  authDomain: "labseu1-db-test.firebaseapp.com",
+  databaseURL: "https://labseu1-db-test.firebaseio.com",
+  projectId: "labseu1-db-test",
+  storageBucket: "labseu1-db-test.appspot.com",
+  messagingSenderId: "280261803917",
+  appId: "1:280261803917:web:3c02fd1e7ac4ae6a"
 };
-
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCCwoDp2h3hUwqn5nGDD3BIRCeGT1A8yWo",
+//   authDomain: "pinely-staging.firebaseapp.com",
+//   databaseURL: "https://pinely-staging.firebaseio.com",
+//   projectId: "pinely-staging",
+//   storageBucket: "pinely-staging.appspot.com",
+//   messagingSenderId: "10729362050",
+//   appId: "1:10729362050:web:59266b15015d5b96f71a9b",
+//   measurementId: "G-HY76MYB9Q1"
+// };
 export default firebaseConfig;
 
-
-export const paymentEndPoint = 'https://us-central1-labseu1-db-test.cloudfunctions.net/app/charge';
-
+export const paymentEndPoint =
+  "https://us-central1-labseu1-db-test.cloudfunctions.net/app/charge";
