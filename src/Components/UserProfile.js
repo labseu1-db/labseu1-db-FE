3a4d497bc189c9fafae537dcff4b017cba25caf4
Thorben <bender.thorben@gmail.com>
2020-05-03 16:48:46 +0200

    removed redux from userprofile

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import ProfileCard from './reusable-components/ProfileCard';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import Navbar from './NavBar';
 import RightSidebar from './RightSidebar';
 
