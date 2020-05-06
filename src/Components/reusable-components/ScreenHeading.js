67ada2172b541ce0ef233e3081c5b5da1170d343
Thorben <bender.thorben@gmail.com>
2020-05-06 21:36:09 +0200

    remove redux from ScreenHeading

--- src/Components/reusable-components/ScreenHeading.js
+++ src/Components/reusable-components/ScreenHeading.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { isEmpty } from 'react-redux-firebase';
 
 function ScreenHeading(props) {
   const { heading, info, topic } = props;
-  if (isEmpty(topic)) {
+  if (!topic) {
     return (
       <div>
         <StyledHeadingContainer>
