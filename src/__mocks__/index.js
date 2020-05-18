941638fc12cac036aa8fca5da60445a6e39a4a2f
Thorben <bender.thorben0@gmail.com>
2020-05-18 16:29:33 +0200

    Add testing for MainScreen

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -1,87 +1,28 @@
-export const user = { fullName: 'Thorben', userEmail: 'thorben@gmail.com' };
-export const orgs = [{ id: 2 }, { id: 284 }];
-export const org = {
-  orgName: 'test org',
-  isPremium: false,
-  createdByUserId: '389549'
-};
-export const heading = 'Test';
-export const info = 'This is a test';
-export const commentBody = 'This is a test comment';
-export const arrayOfUsersWhoLiked = [23, 385, 2845, 38385];
-export const commentCreatedAt = 1558373067909;
-export const commentCreatedByUserId = 32;
-export const commentCreatedByUserName = 'test operator';
-export const commentUpdatedAt = 1558374066638;
-export const commentIsDecided = false;
-export const commentIsUpdated = true;
-export const orgId = 2435;
-export const threadId = 284895;
-export const threadName = 'Test Thread';
-export const fullName = 'Test';
-export const text = 'This is a test text';
-export const users = [
-  { fullName: 'Test', id: 383 },
-  { fullName: 'test 2', id: 38 }
-];
-export const userArray = [385, 8549684, 48956486, 945849];
-export const shoudlBeOpen = true;
-export const spaces = [{ orgId: '22' }, { orgId: '22' }, { orgId: '22' }];
-export const space = { arrayOfUserIdsInSpace: userArray, spaceName: fullName };
-export const teamEmailAddress = [
-  'test1@gmail.com',
-  'test2@gmail.com',
-  'test3@gmail.com',
-  'test4@gmail.com'
-];
-export const threads = [
+// Data
+const spaces = [{ spaceName: 'Test 1' }, { spaceName: 'Test 2' }];
+const orgs = [{ orgName: 'Org 1' }, { orgName: 'Org 2' }];
+const users = [{ fullName: 'Thorben' }, { fullName: 'Sam' }];
+const user = { fullName: 'Thorben' };
+const threads = [
   {
-    id: 22,
-    threadName: 'test',
-    whenUserHasSeen: 30545894,
-    threadTopic: 'Testing',
-    lastCommentCreatedAt: 30548594,
-    spaceId: 389548594594
+    threadName: 'Thread 1',
+    whenUserHasSeen: { null: 22 },
+    threadTopic: 'Testing 1'
+  },
+  {
+    threadName: 'Thread 2',
+    whenUserHasSeen: { null: 23 },
+    threadTopic: 'Testing 2'
   }
 ];
-export const topic = 'Testing';
-export const match = {
-  params: { id: '22' },
-  path: '/mainscreen/:id'
-};
-export const showModal = function() {
-  console.log('Test');
-};
-export const firestore = {
-  add: function() {
-    console.log('Test run');
-  },
-  collection: function(data) {
-    return {
-      doc: function() {
-        return {
-          update: function() {
-            console.log('Should update');
-          }
-        };
-      }
-    };
-  }
-};
-export const auth = {};
-export const firebase = {
-  auth: function() {
-    return {
-      isSignInWithEmailLink: function() {
-        console.log('Test');
-      }
-    };
-  }
-};
-export const history = {
-  goBack: function() {
-    console.log('Go Back test');
-  }
-};
-export const comments = [{ arrayOfUserIdsWhoLiked: userArray, id: '22' }];
-export const resetPasswordStatus = false;
+
+export const setError = () => {};
+export const isLoggedIn = () => {};
+export const loadingFalse = false;
+export const loadingTrue = true;
+export const useMountEffect = callFunction => callFunction;
+export const getUserDataRealTime = setData => () => setData(user);
+export const getSpacesWithOrg = (setData, orgId) => () => setData(spaces);
+export const getOrgWithUuid = (setData, uuid) => () => setData(orgs);
+export const getUsersFromOrg = (setData, orgId) => () => setData(users);
+export const getThreadsWithOrg = (setData, orgId) => () => setData(threads);
---
+++ src/__tests__/Components/MainScreen.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import MainScreen from '../../Components/MainScreen';
+import Context from '../../Components/ContextProvider/Context';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import {
+  setError,
+  isLoggedIn,
+  loadingFalse,
+  useMountEffect,
+  getUserDataRealTime,
+  getSpacesWithOrg,
+  getOrgWithUuid,
+  getUsersFromOrg,
+  getThreadsWithOrg
+} from '../../__mocks__/index';
+import paramCase from 'param-case';
+
+describe('<MainScreen />', () => {
+  it('Render MainScreen', () => {
+    const { getByLabelText } = render(
+      <MemoryRouter>
+        <Context.Provider
+          value={{
+            setError: setError,
+            isLoggedIn: isLoggedIn,
+            loading: loadingFalse,
+            useMountEffect: useMountEffect,
+            getUserDataRealTime: getUserDataRealTime,
+            getSpacesWithOrg: getSpacesWithOrg,
+            getOrgWithUuid: getOrgWithUuid,
+            getUsersFromOrg: getUsersFromOrg,
+            getThreadsWithOrg: getThreadsWithOrg
+          }}
+        >
+          <MainScreen match={{ params: { id: 22 } }} />
+        </Context.Provider>
+      </MemoryRouter>
+    );
+  });
+});
