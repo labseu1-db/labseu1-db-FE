8b136904a924d690afc630abe22a1c4212c7d20b
Thorben <bender.thorben0@gmail.com>
2020-05-26 01:08:05 +0200

    Add testing for UpgradeAccount

---
+++ src/__tests__/Components/UpgradeAccount.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import UpgradeAccount from '../../Components/UpgradeAccount';
+import Context from '../../Components/ContextProvider/Context';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import {
+  getUserDataRealTime,
+  match,
+  getUsersFromOrg,
+  getOrgWithUuid,
+  getOrgWithId,
+  getSpacesWithOrg,
+  resetPasswordStatusFalse
+} from '../../__mocks__/index';
+
+describe('<UpgradeAccount />', () => {
+  it('Render UpgradeAccount', () => {
+    const { getByLabelText } = render(
+      <MemoryRouter>
+        <Context.Provider
+          value={{
+            getOrgWithId: getOrgWithId,
+            getUserDataRealTime: getUserDataRealTime,
+            getUsersFromOrg: getUsersFromOrg,
+            getOrgWithUuid: getOrgWithUuid,
+            getSpacesWithOrg: getSpacesWithOrg
+          }}
+        >
+          <UpgradeAccount match={match} />
+        </Context.Provider>
+      </MemoryRouter>
+    );
+    expect(getByLabelText(/UpgradeAccount/i)).toBeInTheDocument();
+  });
+});
