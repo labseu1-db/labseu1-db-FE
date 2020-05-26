448d578f51ddf7fbe40138ad946e66be2ecaa39e
Thorben <bender.thorben0@gmail.com>
2020-05-26 19:11:56 +0200

    Delete unused dependencies

--- src/__tests__/Components/RightSidebar.test.js
+++ src/__tests__/Components/RightSidebar.test.js
@@ -4,14 +4,6 @@ import RightSidebar from '../../Components/RightSidebar';
 import Context from '../../Components/ContextProvider/Context';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import {
-  loadingTrue,
-  getUserDataRealTime,
-  getSpacesWithOrg,
-  getOrgWithUuid,
-  getUsersFromOrg,
-  match
-} from '../../__mocks__/index';
 
 describe('<RightSidebar', () => {
   it('Render RightSidebar', () => {
