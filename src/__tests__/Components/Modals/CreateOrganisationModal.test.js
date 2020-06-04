ff48e54a8c160144f79dddce6e14e1cd74bfa767
Thorben <bender.thorben0@gmail.com>
2020-06-04 21:45:38 +0200

    Delete Context.Provider from CreateOrganisationModal

--- src/__tests__/Components/Modals/CreateOrganisationModal.test.js
+++ src/__tests__/Components/Modals/CreateOrganisationModal.test.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import CreateOrganisationModal from '../../../Components/Modals/CreateOrganisationModal';
-import Context from '../../../Components/ContextProvider/Context';
 import { render } from '@testing-library/react';
 import { isOpen } from '../../../__mocks__/index';
 
 describe('<CreateOrganisationModal />', () => {
   it('Render CreateOrganisationModal', () => {
     const { getByLabelText } = render(
-      <Context.Provider>
-        <CreateOrganisationModal shoudlBeOpen={isOpen} />
-      </Context.Provider>
+      <CreateOrganisationModal shoudlBeOpen={isOpen} />
     );
     expect(getByLabelText('CreateOrganisationModal')).toBeInTheDocument();
   });
