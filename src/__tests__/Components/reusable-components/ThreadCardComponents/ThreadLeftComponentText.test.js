ebdc03d05e9731ddbfaccb897c654d13292d5603
Thorben <bender.thorben0@gmail.com>
2020-06-10 16:10:21 +0200

    Change test description

--- src/__tests__/Components/reusable-components/ThreadCardComponents/ThreadLeftComponentText.test.js
+++ src/__tests__/Components/reusable-components/ThreadCardComponents/ThreadLeftComponentText.test.js
@@ -3,8 +3,8 @@ import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import ThreadLeftComponentText from '../../../../Components/reusable-components/ThreadCardComponents/ThreadLeftComponentText';
 
-describe('<ThreadLeftComponentImage />', () => {
-  it('Render ThreadLeftComponentImage', () => {
+describe('<ThreadLeftComponentText />', () => {
+  it('Render ThreadLeftComponentText', () => {
     const { getByLabelText } = render(<ThreadLeftComponentText />);
     expect(getByLabelText('ThreadLeftComponentText')).toBeInTheDocument();
   });
