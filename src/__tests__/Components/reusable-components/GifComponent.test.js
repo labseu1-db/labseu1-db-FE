7be6f4c5852ab86ba6a0c750b8ee52a854cd2b1c
Thorben <bender.thorben0@gmail.com>
2020-06-10 17:00:01 +0200

    Delete unused dependencies

--- src/__tests__/Components/reusable-components/GifComponent.test.js
+++ src/__tests__/Components/reusable-components/GifComponent.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, getByLabelText } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import GifComponent from '../../../Components/reusable-components/GifComponent';
-import Context from '../../../Components/ContextProvider/Context';
 
 describe('<GifComponent />', () => {
   it('Render GifComponent', () => {
